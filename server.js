f34cdbcf4aa4e4e8d2c632a1818191274f43ba2e
Lakshya <lakshyagarg13200132@gmail.com>
2022-11-19 20:45:52 +0530

    added Procfile etc

--- server.js
+++ server.js
@@ -32,6 +32,11 @@ app.post("/",function(req,res){
     res.sendFile(__dirname +"/index.html");
 });
 
-app.listen(3000,function(){
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port,function(){
     console.log("Server started");
 });
